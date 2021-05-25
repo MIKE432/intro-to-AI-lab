f93b6972a930df8386a60ded1cfa522b1deab056
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2021-05-25 09:23:50 +0200

    added parameters tuning

--- TextClassification/LoadData.py
+++ TextClassification/LoadData.py
@@ -1,13 +1,15 @@
 import random
 
+from sklearn import svm
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, CountVectorizer
 from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import numpy as np
 
 
-def get_sets(train_percentage, verified_percentage):
+def get_sets():
     texts1 = open("scaledata/Dennis+Schwartz/subj.Dennis+Schwartz", "r")
     texts2 = open("scaledata/James+Berardinelli/subj.James+Berardinelli", "r")
     texts3 = open("scaledata/Scott+Renshaw/subj.Scott+Renshaw", "r")
@@ -28,28 +30,36 @@ def get_sets(train_percentage, verified_percentage):
     labels.extend(raw_labels3.readlines())
     labels.extend(raw_labels4.readlines())
 
-    return get_training_and_test_set(lines, labels, train_percentage)
+    return lines, labels
 
 
-def predict_svm(X_train, Y_train, X_test, Y_test):
+def predict_svm(X_train, Y_train, X_test, Y_test, **kwargs):
     text_clf = Pipeline([
         ('vect', CountVectorizer(stop_words='english')),
         ('tfidf', TfidfTransformer()),
-        ('clf', SGDClassifier(loss='hinge', penalty='l2',
-                              alpha=1e-3, random_state=42,
-                              max_iter=5, tol=None)),
+        ('clf', svm.SVC(kernel='linear')),
+    ])
+    text_clf.set_params(**kwargs)
+    text_clf.fit(X_train, Y_train)
+    predicted = text_clf.predict(X_test)
+    return np.mean(predicted == Y_test)
+
+
+def predict_bayes(X_train, Y_train, X_test, Y_test, alpha=1.0, fit_prior=True):
+    text_clf = Pipeline([
+        ('vect', CountVectorizer(stop_words='english')),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultinomialNB(alpha=alpha, fit_prior=fit_prior)),
     ])
 
     text_clf.fit(X_train, Y_train)
     predicted = text_clf.predict(X_test)
-    print(np.mean(predicted == Y_test))
+    return np.mean(predicted == Y_test)
 
 
 def get_training_and_test_set(lines, labels, percentage=0.9):
     zipped = list(zip(lines, labels))
-
     random.shuffle(zipped)
-
     x = len(zipped)
     a = int(x * percentage)
     train_set_zipped = zipped[:a]
@@ -58,23 +68,72 @@ def get_training_and_test_set(lines, labels, percentage=0.9):
     return list(zip(*train_set_zipped)), list(zip(*test_set_zipped))
 
 
-def get_vocabulary(words):
-    d = {}
+def pick_train_and_test_sets(X, Y, k, nfolds):
+    sets = split_sets_to_smaller_pieces(X, Y, nfolds)
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
 
-    for word in words:
-        if word not in d:
-            d[word] = 0
+    for i in range(len(sets)):
+        if i != k:
+            X_train.extend(sets[i][0])
+            Y_train.extend(sets[i][1])
         else:
-            d[word] += 1
+            X_test.extend(sets[i][0])
+            Y_test.extend(sets[i][1])
 
-    return d
+    return X_train, Y_train, X_test, Y_test
 
 
-def trim_vocabulary(vocabulary, top_offset, bottom_offset):
-    l = len(vocabulary.keys())
-    new_d = {}
-    for key in vocabulary.keys():
-        if l * bottom_offset > vocabulary[key] or vocabulary[key] > l * top_offset:
-            new_d[key] = vocabulary[key]
+def tuning_hyper_parameters_nb(X, Y, alphas, fit_priors, nfolds):
+    best_params = alphas[0], fit_priors[0]
+    best = np.inf * -1
 
-    return new_d
+    for alpha in alphas:
+        for fit_prior in fit_priors:
+            vals = []
+            for i in range(0, nfolds):
+                X_train, Y_train, X_text, Y_test = pick_train_and_test_sets(X, Y, i, nfolds)
+                vals.append(predict_bayes(X_train, Y_train, X_text, Y_test, alpha, fit_prior))
+
+            val = np.mean(vals)
+            if val > best:
+                best = val
+                print(best)
+                best_params = alpha, fit_prior
+
+    return best_params
+
+
+def tuning_hyper_parameters_svm(X, Y, cs, gammas, nfolds):
+    best_params = {}
+    best = np.inf * -1
+
+    for c in cs:
+        for gamma in gammas:
+            vals = []
+            params = {'clf__C': c, 'clf__gamma': gamma}
+            for i in range(0, nfolds):
+                X_train, Y_train, X_text, Y_test = pick_train_and_test_sets(X, Y, i, nfolds)
+                vals.append(predict_svm(X_train, Y_train, X_text, Y_test, **params))
+
+            val = np.mean(vals)
+            if val > best:
+                best = val
+                best_params = params
+
+    return best_params
+
+
+def split_sets_to_smaller_pieces(X, Y, k):
+    zipped = list(zip(X, Y))
+    random.shuffle(zipped)
+
+    length = len(zipped)
+    part = int(length / k)
+    parts = []
+    for i in range(1, k + 1):
+        parts.append(zipped[part * (i - 1):i * part])
+
+    return list(map(lambda _x: list(zip(*_x)), parts))
--- TextClassification/Main.py
+++ TextClassification/Main.py
@@ -1,5 +1,13 @@
-from LoadData import get_sets, predict_svm
+from LoadData import get_sets, predict_svm, get_training_and_test_set, split_sets_to_smaller_pieces, \
+    tuning_hyper_parameters_svm, tuning_hyper_parameters_nb
 
 if __name__ == '__main__':
-    [X_train, Y_train], [X_test, Y_test] = get_sets(0.9, 0.2)
+    X, Y = get_sets()
+    s = split_sets_to_smaller_pieces(X, Y, 10)
+    Cs = [0.001, 0.01, 0.1, 1, 10]
+    gammas = [0.001, 0.01, 0.1, 1]
+    # tuning_hyper_parameters_svm(X, Y, Cs, gammas, 10)
+    tuning_hyper_parameters_nb(X, Y, [0.01, 0.1, 0.2, 0.5, 0.7, 0.9, 1.0], [True, False], 10)
+    [X_train, Y_train], [X_test, Y_test] = get_training_and_test_set(X, Y)
     predict_svm(X_train, Y_train, X_test, Y_test)
+
